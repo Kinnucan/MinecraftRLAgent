68f2b82f4ebf52ca68290810b6f61c3fed1cd7d9
Kinnucan <ryan.kinnucan@gtt.com>
2022-04-17 18:44:46 -0500

    Modified params, experimented with movement commands

--- MarLO/SingleAgent/CliffWalking/agent.py
+++ MarLO/SingleAgent/CliffWalking/agent.py
@@ -1,23 +1,34 @@
 import marlo
+import time
+
 client_pool = [('127.0.0.1', 10000)]
 join_tokens = marlo.make('MarLo-CliffWalking-v0',
-                          params={
-                            "client_pool": client_pool
-                          })
-# As this is a single agent scenario,
-# there will just be a single token
+                        params={
+                            "client_pool": client_pool,
+                            "allowContinuousMovement": True,
+                            "allowDiscreteMovement": True,
+                            "allowAbsoluteMovement": True,
+                        })
+
 assert len(join_tokens) == 1
 join_token = join_tokens[0]
 
 env = marlo.init(join_token)
-
-observation = env.reset()
+env.reset()
 
 done = False
 while not done:
-  _action = env.action_space.sample()
-  obs, reward, done, info = env.step(_action)
-  print("reward:", reward)
-  print("done:", done)
-  print("info", info)
+    # _action = env.action_space.sample()
+    # print(_action)
+
+    # env.send_command("move 1")
+    env.send_command("turn 1")
+    # time.sleep(1)
+    # done = True
+
+    # obs, reward, done, info = env.step(0)
+    # print("reward:", reward)
+    # print("done:", done)
+    # print("info", info)
+
 env.close()
