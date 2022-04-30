7319a1618169fbf2cd7771b5974cde6938de60d3
Kinnucan <ryan.kinnucan@gtt.com>
2022-04-30 09:22:05 -0500

    Updated custom lava maze, finished walking agent and dijkstra agent

--- MarLO/SingleAgent/CliffWalking/dijkstra_agent.py
+++ MarLO/SingleAgent/CliffWalking/dijkstra_agent.py
@@ -1,10 +1,9 @@
+from tracemalloc import start
 import marlo
 import time
 import sys
 import numpy as np
 np.set_printoptions(threshold=sys.maxsize)
-
-# 'C:\\Users\\Ryan.Kinnucan\\MinecraftRLAgent\\MarLO\\Missions\\lava_maze.xml'
 
 # Debug
 log = True
@@ -15,7 +14,7 @@ goal_coords = (29, 40)
 curr_coords = start_coords
 # Stores all nodes that have and have not been explored, respectively
 explored = []
-unexplored = [start_coords]
+frontier = [start_coords]
 # Stores cost for the agent to move from the start node to the given node
 node_costs = { start_coords: 0 }
 # Stores the immediate ancestor node of the node listed as the key
@@ -27,10 +26,10 @@ map = np.array([[-1] * 80] * 80)
 
 # Initialize Environment
 client_pool = [('127.0.0.1', 10000)]
-join_tokens = marlo.make('MarLo-CliffWalking-v0',
+join_tokens = marlo.make('C:\\Users\\Ryan.Kinnucan\\MinecraftRLAgent\\MarLO\\Missions\\lava_maze.xml',
                         params={
                             "client_pool": client_pool,
-                            "tick_length": 30,
+                            "max_retries": 100,
                             "agent_names": ["MarLo-Agent-0"],
                             "allowContinuousMovement": True,
                             "allowDiscreteMovement": True,
@@ -61,10 +60,14 @@ env.reset()
 
 # Given the agent's current location and a destination,
 # send the agent to that destination
-def walk_to_node(curr, dest):
-    if (curr == dest): return
+def walk_to_node(start, dest, reverse=True):
+    if (start == dest): return
 
-    path = reconstruct_path(curr, dest)
+    path = reconstruct_path(start, dest, reverse)
+    if log:
+        print("--------------------")
+        print("Path:", path)
+        print("--------------------")
 
     for i in range(0, len(path) - 1):
         curr = path[i]
@@ -80,25 +83,59 @@ def walk_to_node(curr, dest):
         env.send_command(command)
         time.sleep(0.5)
 
-# Given a starting node and a destination, reconstruct the path
-# to get from the start to the destination based on prev_node
-def reconstruct_path(start, dest):
-    curr_node = dest
+# Reconstruct the agent's path from the start location to the destination. 
+# Start from the destination and work backwards, then reverse if necessary
+def reconstruct_path(start, dest, reverse):
+    if (reverse): curr_node = dest
+    else: curr_node = start
     path = [curr_node]
     prev = prev_node[curr_node]
-    while prev != start:
-        path.append(prev)
-        curr_node = prev
-        prev = prev_node[curr_node]
-    return path.reverse()
+
+    if log:
+        print("--------------------")
+        print("Reconstructing Path")
+        print("Start Node:", start)
+        print("Destination Node:", dest)
+
+    if (reverse):
+        while (prev != start and prev != None):
+            # if log:
+            #     print("--------------------")
+            #     print("Curr Node:", curr_node)
+            #     print("Prev:", prev)
+            path.append(prev)
+            curr_node = prev
+            prev = prev_node[curr_node]
+    else:
+        while (prev != dest):
+            # if log:
+                # print("--------------------")
+                # print("Curr Node:", curr_node)
+                # print("Prev:", prev)
+            path.append(prev)
+            curr_node = prev
+            prev = prev_node[curr_node]
+
+    if (reverse):
+        path.append(start)
+        path.reverse()
+    else:
+        path.append(dest)
+
+    return path
 
 # While frontier is not empty
-while bool(unexplored):
+while bool(frontier):
     # If agent falls into lava, the environment must
     # be reset so that the agent will respawn
 
-    new_coords = unexplored.pop(0)
+    new_coords = frontier.pop(0)
     cost = node_costs[new_coords]
+
+    if log:
+        print("--------------------")
+        print("New Coordinates:", new_coords)
+        print("Cost:", cost)
 
     walk_to_node(curr_coords, new_coords)
 
@@ -106,14 +143,13 @@ while bool(unexplored):
     obs, reward, done, info = env.step(0)
 
     if (reward == -100): 
-        map[curr_coords[0], curr_coords[1]] = -100
+        map[new_coords[0], new_coords[1]] = -100
         env.reset()
-        time.sleep(2)
+        time.sleep(1)
         walk_to_node(start_coords, curr_coords)
     if (reward != -100):
-        map[curr_coords[0], curr_coords[1]] = reward
-
         curr_coords = new_coords
+        map[curr_coords[0], curr_coords[1]] = reward
         explored.append(curr_coords)
 
         agent_row = curr_coords[0]
@@ -125,30 +161,42 @@ while bool(unexplored):
                         (agent_row, agent_col + 1)
                     ]
 
-        for n in neighbors:
-            new_cost = 1
-            total_cost = cost + new_cost
-            if ((n not in explored) and (n not in unexplored)):
-                unexplored.append(n)
-                node_costs[n] = total_cost
-                prev_node[n] = curr_coords
-            elif (n not in explored):
-                if (total_cost < node_costs[n]): 
+        if log:
+            print("Neighbors:", neighbors)
+
+        # If the goal node is found, the agent doesn't need
+        # to explore any of the blocks around it unless there
+        # is a path which leads to said blocks that doesn't
+        # go through the goal node
+        if (reward != 100):
+            for n in neighbors:
+                new_cost = 1
+                total_cost = cost + new_cost
+                if ((n not in explored) and (n not in frontier)):
+                    frontier.append(n)
                     node_costs[n] = total_cost
-                prev_node[n] = curr_coords
-        unexplored.sort(reverse=True)
+                    prev_node[n] = curr_coords
+                elif (n not in explored):
+                    if (total_cost < node_costs[n]): 
+                        node_costs[n] = total_cost
+                        prev_node[n] = curr_coords
+
+        walk_to_node(curr_coords, start_coords, False)
+        curr_coords = start_coords
 
     if log:
         print("reward:", reward)
         print("done:", done)
         print("info", info)
+        print("map:")
+        print(map[27:43, 38:46])
 
 if (goal_coords in explored):
     env.reset()
-    time.sleep(2)
     walk_to_node(start_coords, goal_coords)
     obs, reward, done, info = env.step(0)
-    print(reward)
+    print("Final Reward:", reward)
+    print("Final Cost:", node_costs[goal_coords])
 
 env.close()
 print(map[27:43, 38:46])
--- MarLO/SingleAgent/CliffWalking/walking_agent.py
+++ MarLO/SingleAgent/CliffWalking/walking_agent.py
@@ -4,8 +4,6 @@ import sys
 import numpy as np
 np.set_printoptions(threshold=sys.maxsize)
 
-# 'C:\\Users\\Ryan.Kinnucan\\MinecraftRLAgent\\MarLO\\Missions\\lava_maze.xml'
-
 # Debug
 log = True
 
@@ -24,10 +22,10 @@ map = np.array([[-1] * 80] * 80)
 
 # Initialize Environment
 client_pool = [('127.0.0.1', 10000)]
-join_tokens = marlo.make('MarLo-CliffWalking-v0',
+join_tokens = marlo.make('C:\\Users\\Ryan.Kinnucan\\MinecraftRLAgent\\MarLO\\Missions\\lava_maze.xml',
                         params={
                             "client_pool": client_pool,
-                            "tick_length": 30,
+                            "max_retries": 100,
                             "agent_names": ["MarLo-Agent-0"],
                             "allowContinuousMovement": True,
                             "allowDiscreteMovement": True,
@@ -40,6 +38,8 @@ env = marlo.init(join_token)
 env.reset()
 
 obs, reward, done, info = env.step(0)
+map[start_coords[0], start_coords[1]] = reward
+explored.append(start_coords)
 
 # Commands (W/ Indicators of Reliability):
 
@@ -60,10 +60,14 @@ obs, reward, done, info = env.step(0)
 
 # Given the agent's current location and a destination,
 # send the agent to that destination
-def walk_to_node(curr, dest):
-    if (curr == dest): return
+def walk_to_node(start, dest, reverse=True):
+    if (start == dest): return
 
-    path = reconstruct_path(curr, dest)
+    path = reconstruct_path(start, dest, reverse)
+    if log:
+        print("--------------------")
+        print("Path:", path)
+        print("--------------------")
 
     for i in range(0, len(path) - 1):
         curr = path[i]
@@ -79,31 +83,129 @@ def walk_to_node(curr, dest):
         env.send_command(command)
         time.sleep(0.5)
 
-def reconstruct_path(start, dest):
-    curr_node = dest
+# Reconstruct the agent's path from the start location to the destination. 
+# Start from the destination and work backwards, then reverse if necessary
+def reconstruct_path(start, dest, reverse):
+    if (reverse): curr_node = dest
+    else: curr_node = start
     path = [curr_node]
     prev = prev_node[curr_node]
-    while prev != start:
-        path.append(prev)
-        curr_node = prev
-        prev = prev_node[curr_node]
-    return path.reverse()
+
+    if log:
+        print("--------------------")
+        print("Reconstructing Path")
+        print("Start Node:", start)
+        print("Destination Node:", dest)
+
+    if (reverse):
+        while (prev != start and prev != None):
+            # if log:
+            #     print("--------------------")
+            #     print("Curr Node:", curr_node)
+            #     print("Prev:", prev)
+            path.append(prev)
+            curr_node = prev
+            prev = prev_node[curr_node]
+    else:
+        while (prev != dest):
+            # if log:
+                # print("--------------------")
+                # print("Curr Node:", curr_node)
+                # print("Prev:", prev)
+            path.append(prev)
+            curr_node = prev
+            prev = prev_node[curr_node]
+
+    if (reverse):
+        path.append(start)
+        path.reverse()
+    else:
+        path.append(dest)
+
+    return path
+
+# Given a list of neighboring nodes and the
+# agent's current location, explore all neighbors
+def explore_neighbors(neighbors, curr_coords):
+    for n in neighbors:
+        if (n not in explored):
+            prev_node[n] = curr_coords
+
+            if log:
+                print("--------------------")
+                print("Current Neighbor:", n)
+                print("Neighbor's Prev:", prev_node[n])
+
+            walk_to_node(curr_coords, n)
+
+            # Get observation
+            time.sleep(1)
+            obs, reward, done, info = env.step(0)
+
+            if log:
+                print("--------------------")
+                print("reward:", reward)
+                print("done:", done)
+                print("info", info)
+
+            # If agent falls into lava, the environment must
+            # be reset so that the agent will respawn, after
+            # which the agent needs to be returned to the
+            # last location it was exploring from
+            if (reward == -100): 
+                map[n[0], n[1]] = -100
+
+                if log:
+                    print("map:")
+                    print(map[27:43, 38:46])
+                    print("--------------------")
+                    print("Reseting Env")
+                    print("--------------------")
+
+                env.reset()
+
+                explored.append(n)
+
+                # Remove lava nodes from frontier because 
+                # their neighbors will be inaccessible
+                frontier.remove(n)
+
+                # Walk the agent back to its previous location
+                # if there are still neighbors to explore
+                if (bool(neighbors)): walk_to_node(start_coords, curr_coords)
+
+            # If agent does not fall into lava, set the node
+            # at that location equal to the reward, then walk
+            # the agent back to curr_coords
+            if (reward != -100):
+                map[n[0], n[1]] = reward
+
+                # If the goal node is found, the agent doesn't need
+                # to explore any of the blocks around it unless there
+                # is a path which leads to said blocks that doesn't
+                # go through the goal node
+                if (reward == 100): frontier.remove(n)
+
+                if log:
+                    print("map:")
+                    print(map[27:43, 38:46])
+                    
+                explored.append(n)
+                walk_to_node(n, curr_coords, False)
 
 # While frontier is not empty
 while bool(frontier):
-    # If agent falls into lava, the environment must
-    # be reset so that the agent will respawn
-    if (reward == -100): 
-        map[curr_coords[0], curr_coords[1]] = -100
-        env.reset()
-        time.sleep(2)
+    # Walk agent to the first node in the frontier
+    dest = frontier.pop(0)
+    if log:
+        print("--------------------")
+        print("Node to Explore:", dest)
+        print("Current Node:", curr_coords)
+    walk_to_node(curr_coords, dest)
+    curr_coords = dest
 
-    if (reward == 100): map[curr_coords[0], curr_coords[1]] = 100
-    if (reward == 0): map[curr_coords[0], curr_coords[1]] = 0
-
-    explored.append(curr_coords)
-    frontier.remove(curr_coords)
-
+    # Get agent's current row and column
+    # and all of its neighbors
     agent_row = curr_coords[0]
     agent_col = curr_coords[1]
     neighbors = [
@@ -113,26 +215,22 @@ while bool(frontier):
                     (agent_row, agent_col + 1)
                 ]
 
-    # If a neighboring nodes has not already been 
-    # explored, then add it to the frontier
+    # If a neighboring node has not already been explored, and
+    # is not already in the frontier, then add it to the frontier
     for n in neighbors:
         if ((n not in explored) and (n not in frontier)): frontier.append(n)
-
-    # If frontier is not empty, send the agent to
-    # the next unexplored node
-    if bool(frontier):
-        dest = frontier[0]
-        prev_node[dest] = curr_coords
-        walk_to_node(curr_coords, dest)
-        curr_coords = dest
-
-    time.sleep(1)
-    obs, reward, done, info = env.step(0)
-
     if log:
-        print("reward:", reward)
-        print("done:", done)
-        print("info", info)
+        print("Frontier:", frontier)
+        print("Neighbors:", neighbors)
+
+    # Explore all neighbors of the current node, then
+    # walk the agent back to its starting location
+    explore_neighbors(neighbors, curr_coords)
+    walk_to_node(curr_coords, start_coords, False)
+
+    # Once a node has been fully explored, remove it from the frontier
+    if (curr_coords in frontier): frontier.remove(curr_coords)
+    curr_coords = start_coords
 
 env.close()
 print(map[27:43, 38:46])
